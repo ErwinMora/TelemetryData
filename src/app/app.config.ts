cdfe632746e80e0bec5244608edb9ee398ed7a8a
Erwin Mora <erwinmm17@outlook.com>
2025-12-01 11:49:35 -0600

    Implemetación de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,23 +6,30 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './core/auth/auth.interceptor';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    providePrimeNG({
-            theme: {
-                options: {
-                    prefix: 'p',
-                    darkModeSelector: 'system',
-                    cssLayer: false
-                }
-            }
-        }), provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  MessageService ,
+  provideAnimationsAsync(),
+  providePrimeNG({
+    theme: {
+      options: {
+        prefix: 'p',
+        darkModeSelector: 'system',
+        cssLayer: false
+      }
+    }
+  }), provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
+  provideHttpClient(),
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ]
 };
 
 
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// PrimeNG Imports
 import { InputGroup } from 'primeng/inputgroup';
 import { InputGroupAddon } from 'primeng/inputgroupaddon';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { PasswordModule } from 'primeng/password';
-import { RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../../../core/auth/auth.service'; // Importamos el AuthService
 
 @Component({
   selector: 'app-login',
-  imports: [
-    InputGroup,
-    InputGroupAddon,
-    InputGroupModule,
-    InputGroupAddonModule,
-    PasswordModule, 
-    RouterLink
-  ],
+  imports: [InputGroup, InputGroupAddon, InputGroupModule, InputGroupAddonModule, PasswordModule, FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private _authService: AuthService, private _router: Router, private _messageService: MessageService) { } // Inyectamos el AuthService y MessageService
 
+  // Variables del formulario
+  email = '';
+  password = '';
+
+  login() { // Método para manerjar el login
+    if (!this.email || !this.password) {
+      this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Favor de completar todos los campos requeridos.' });
+      return;
+    }
+
+    this._authService.login({ email: this.email, password: this.password })
+      .subscribe({
+        next: () => {
+          this._messageService.add({ severity: 'success', summary: 'Éxito', detail: 'Inicio de sesión correcto.' });
+          this._router.navigate(['/graphs/weather']);
+        },
+        error: () => {
+          this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Credenciales incorrectas.' });
+        }
+      });
+  }
 }
