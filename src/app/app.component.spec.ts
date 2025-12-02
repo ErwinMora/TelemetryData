f5a231bbaf0fd6c59634cc75d56999b560ce7a73
Erwin Mora <erwinmm17@outlook.com>
2025-12-02 15:56:40 -0600

    Cambios nuevos package

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'data-telemerty' title`, () => {
+  it(`should have the 'data-telemetry' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('data-telemerty');
+    expect(app.title).toEqual('data-telemetry');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, data-telemerty');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, data-telemetry');
   });
 });
